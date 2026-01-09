77ba43a62137c0f14fc24e107da4ab763c6c5ec9
Niclas Luick <niclas.luick@gmail.com>
2026-01-09 01:32:56 +0100

    excluded files beyond training and classification evals

--- experiments/paper_evals.sh
+++
@@ -1,5 +0,0 @@
-python experiments/classification_eval.py ;
-python experiments/gender_open_ended_eval.py ;
-python experiments/taboo_open_ended_eval.py ;
-python experiments/ssc_open_ended_eval.py ;
-python experiments/personaqa_open_ended_eval.py ;
